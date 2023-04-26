ff0ba7f2d206b5642bff5583e5116f6ec4d594b1
SofLibert <zhilinskij.youtube@mail.ru>
2023-04-27 02:13:27 +0300

    Добавил возможность считывания строк из файла input.txt и вывода результата в консоль.

--- Calculator.java
+++ Calculator.java
@@ -1,39 +1,47 @@
 import java.util.Scanner;
+import java.io.*;
 
 class Calculator{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        String str = sc.nextLine();
-        String[] numbers = str.split(" ");
+    public static void main(String[] args) {
 
-        try{
-            double x = Double.parseDouble(numbers[0]);
-            double y = Double.parseDouble(numbers[2]);
-            if (!numbers[1].equals("+") && !numbers[1].equals("-") && !numbers[1].equals("*") && !numbers[1].equals("/")){
-                System.out.println("Operation Error!");
-                return;
+        try(FileReader reader = new FileReader("input.txt")) {
+            Scanner sc = new Scanner(reader);
+            while (sc.hasNextLine()) {
+                try {
+                    String str = sc.nextLine();
+                    String[] numbers = str.split(" ");
+                    double x = Double.parseDouble(numbers[0]);
+                    double y = Double.parseDouble(numbers[2]);
+                    if (!numbers[1].equals("+") && !numbers[1].equals("-") && !numbers[1].equals("*") && !numbers[1].equals("/")) {
+                        System.out.println("Operation Error!");
+                        continue;
+                    }
+                    if (numbers[1].equals("+")) {
+                        System.out.println(x + y);
+                        continue;
+                    }
+                    if (numbers[1].equals("-")) {
+                        System.out.println(x - y);
+                        continue;
+                    }
+                    if (numbers[1].equals("*")) {
+                        System.out.println(x * y);
+                        continue;
+                    }
+                    if (y == 0) {
+                        System.out.println("Error! Division by zero");
+                        continue;
+                    }
+                    System.out.println(x / y);
+                }
+                catch(NumberFormatException ex) {
+                    System.out.println("Error! Not number");
+                }
             }
-            if (numbers[1].equals("+")){
-                System.out.println(x + y);
-                return;
-            }
-            if (numbers[1].equals("-")){
-                System.out.println(x - y);
-                return;
-            }
-            if (numbers[1].equals("*")){
-                System.out.println(x * y);
-                return;
-            }
-            if (y == 0){
-                System.out.println("Error! Division by zero");
-                return;
-            }
-            System.out.println(x / y);
         }
 
-        catch(NumberFormatException ex){
-            System.out.println("Error! Not number");
+        catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
