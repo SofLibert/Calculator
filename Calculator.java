2d8671391d75ac8f21f2a2f0122ef14458ca83cc
SofLibert <zhilinskij.youtube@mail.ru>
2023-04-27 03:53:16 +0300

    Загрузил готовую версию калькулятора, который получает выражение из одного файла и записывает ответ в другом.

--- Calculator.java
+++ Calculator.java
@@ -4,38 +4,42 @@ import java.io.*;
 class Calculator{
     public static void main(String[] args) {
 
-        try(FileReader reader = new FileReader("input.txt")) {
+        try{
+            FileReader reader = new FileReader("input.txt");
+            FileOutputStream outputStream = new FileOutputStream("output.txt");
+            PrintStream printStream = new PrintStream(outputStream);
             Scanner sc = new Scanner(reader);
+            String str = null;
             while (sc.hasNextLine()) {
                 try {
-                    String str = sc.nextLine();
+                    str = sc.nextLine();
                     String[] numbers = str.split(" ");
                     double x = Double.parseDouble(numbers[0]);
                     double y = Double.parseDouble(numbers[2]);
                     if (!numbers[1].equals("+") && !numbers[1].equals("-") && !numbers[1].equals("*") && !numbers[1].equals("/")) {
-                        System.out.println("Operation Error!");
+                        printStream.println(str + " = " + "Operation Error!");
                         continue;
                     }
                     if (numbers[1].equals("+")) {
-                        System.out.println(x + y);
+                        printStream.println(str + " = " + (x + y));
                         continue;
                     }
                     if (numbers[1].equals("-")) {
-                        System.out.println(x - y);
+                        printStream.println(str + " = " + (x - y));
                         continue;
                     }
                     if (numbers[1].equals("*")) {
-                        System.out.println(x * y);
+                        printStream.println(str + " = " + (x * y));
                         continue;
                     }
                     if (y == 0) {
-                        System.out.println("Error! Division by zero");
+                        printStream.println(str + " = " + "Error! Division by zero");
                         continue;
                     }
-                    System.out.println(x / y);
+                    printStream.println(str + " = " + (x / y));
                 }
                 catch(NumberFormatException ex) {
-                    System.out.println("Error! Not number");
+                    printStream.println(str + " = " + "Error! Not number");
                 }
             }
         }
